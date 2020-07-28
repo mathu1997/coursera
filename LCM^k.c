a01d70e348bd95cc7b182f27553809aafb91c9a9
mathu1997 <30770898+mathu1997@users.noreply.github.com>
2020-07-28 15:25:00 +0530

    Create LCM^k.c
    LCM^k hackerearth problem

---
+++ LCM^k.c
@@ -0,0 +1,26 @@
+lli GCD(lli a,lli b){
+	if(b==0) return a;
+	return GCD(b, a % b); 
+}
+
+lli LCM(lli *arr,lli n)
+{
+	lli ans = abs(arr[0]);
+	for(int i=1;i<n;i++)
+		ans = (abs(arr[i])*ans)/(GCD(ans,abs(arr[i])));
+	return ans;
+}
+int main(){
+	lli test;
+	scanf("%lld",&test);
+	while(test--){
+		lli n,m,k,*arr,lcm;
+		int len;
+		scanf("%lld %lld %lld",&n,&m,&k);
+		arr = (lli *)malloc(n*sizeof(lli));
+		for(int i=0;i<n;i++) scanf("%lld\n",&arr[i]);
+		lcm = LCM(arr,n);
+		printf("%lld\n",(lli)pow(lcm%m,k)%m);
+		// printf("%lld",power(lcm,k,m));
+	}
+}
